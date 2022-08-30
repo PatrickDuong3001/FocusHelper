4b343bd08984ee224dfc4cd51ba561c75bdd39d5
PatrickDuong3001 <baophuc2000@gmail.com>
2022-08-30 01:09:27 -0400

    created a timer for various usage. Also, initilize all widgets of the application

---
+++ customTimer.py
@@ -0,0 +1,20 @@
+from PyQt5 import QtTest
+from PyQt5.QtWidgets import QPlainTextEdit
+
+class customTimer():
+    '''
+    this class is used to pause the program 
+    '''
+    def __init__(self,duration=1000,dialogBox=None,msg=None,final_msg=None):   
+        super().__init__()
+        if dialogBox == None:
+            QtTest.QTest.qWait(duration)
+        else:
+            self.message = "" if msg == None else msg
+            self.dialogBox = dialogBox
+
+            
+            self.dialogBox.appendPlainText(msg)
+            QtTest.QTest.qWait(duration)
+            self.dialogBox.appendPlainText(final_msg)
+            
--- mainProgram.py
+++ mainProgram.py
@@ -1,13 +1,40 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QLineEdit
+from PyQt5.QtWidgets import QMainWindow, QApplication,  QListView, QDateTimeEdit, QSlider, QPushButton, QListWidget, QPlainTextEdit
+from PyQt5.QtGui import QIcon
 from PyQt5 import uic
+from PyQt5 import QtCore
+from customTimer import customTimer
 import sys
 import os
 
+#Initiate UI
 class UI(QMainWindow):
     def __init__(self):
         super(UI,self).__init__()
-        uic.loadUi("FocusUI.ui",self)
+        uic.loadUi("resources/FocusUI.ui",self)
+        self.setFixedSize(915, 616)
+        self.setIcon()
         self.show() 
+        
+        #define Widgets
+        self.dialogBox = self.findChild(QPlainTextEdit,"dialogBox")
+        self.listApps = self.findChild(QListView,"listApps")
+        self.durationSetter = self.findChild(QSlider,"durationSetter")
+        self.lockActivate = self.findChild(QPushButton,"lockActivate")
+        self.listStatus = self.findChild(QListWidget,"listStatus")
+        self.dateTime = self.findChild(QDateTimeEdit,"dateTime")
+        self.dateTime.setDateTime(QtCore.QDateTime.currentDateTime())
+        
+        #initial dialog messages
+        self.dialogBox.appendPlainText("Welcome to FocusHelper. Hope you enjoy it! :)")
+        customTimer()
+        customTimer(3000,self.dialogBox,"Detecting applications...","Done!")
+
+        
+    def setIcon(self):
+        appIcon = QIcon("resources/focusIcon.png")
+        self.setWindowIcon(appIcon)
+
+
         
 # Initialize the app
 app = QApplication(sys.argv)
