6ccb5178a42e88f4a36a87c1e0404d6282936216
PatrickDuong3001 <baophuc2000@gmail.com>
2022-09-03 02:23:39 -0400

    create a dialog box to tell the users to choose enough values to activate countdown timer for apps. Also, able to grey out an already chosen app from the list

--- mainProgram.py
+++ mainProgram.py
@@ -1,5 +1,6 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QDateTimeEdit, QSlider, QPushButton, QListWidget, QPlainTextEdit, QListWidgetItem
+from PyQt5.QtWidgets import QMainWindow, QApplication, QDateTimeEdit, QSlider, QPushButton, QListWidget, QPlainTextEdit, QMessageBox
 from PyQt5.QtGui import QIcon
+from functools import partial
 from PyQt5 import QtGui
 from PyQt5 import uic
 from PyQt5 import QtCore
@@ -28,18 +29,27 @@ class UI(QMainWindow):
         self.listApps2 = self.findChild(QListWidget,"listApps2")
         self.durationSetter = self.findChild(QSlider,"durationSetter")
         self.activate1 = self.findChild(QPushButton,"activate1")
-        self.activate2 = self.findChild(QPushButton,"activate1")
+        self.activate2 = self.findChild(QPushButton,"activate2")
         self.dateTime = self.findChild(QDateTimeEdit,"dateTime")
         self.dateTime.setDateTime(QtCore.QDateTime.currentDateTime())
         
+        #controls
+        self.activate1.setEnabled(False)
+        self.activate2.setEnabled(False)
+        self.timeStopChosen = False
+        self.anAppChosenToStop = False
+        
         #events
-        #self.listApps.itemClicked.connect(self.updateList)
-        self.activate1.clicked.connect(self.activatedCountDown1)
+        self.activate2.clicked.connect(self.activatedCountDown)
+        self.dateTime.dateTimeChanged.connect(partial(self.setControl,1))
+        self.listApps2.itemClicked.connect(partial(self.setControl,2))
         
         #initial dialog messages
         self.dialogBox.appendPlainText("Welcome to FocusHelper. Hope you enjoy it! :)")
         customTimer()
-        customTimer(3000,self.dialogBox,"Detecting applications...","Done!")
+        customTimer(1000,self.dialogBox,"Detecting applications...","Done!")
+        self.activate1.setEnabled(True)
+        self.activate2.setEnabled(True)
         
         #display list of apps 
         self.appDetect1 = appDetector(self.listApps)
@@ -47,26 +57,41 @@ class UI(QMainWindow):
         self.appDetect1.appDetect()
         self.appDetect2.appDetect()
         
-
-    def activatedCountDown1(self):
-        items = self.listApps.selectedItems()
-        for item in items:
-            self.timedApps.append(item.text())
-        self.appDetect1.updateListAppView(self.timedApps)
-
-    
-        
-        
     def setIcon(self):
         appIcon = QIcon("resources/focusIcon.png")
         self.setWindowIcon(appIcon)
+        
+    def setControl(self,controlType):
+        if controlType == 1: #timeStopChosen signal
+            self.timeStopChosen = True 
+        elif controlType == 2: #anAppChosenToStop signal
+            self.anAppChosenToStop = True
+    
+    def activatedCountDown(self):
+        if (self.anAppChosenToStop and self.timeStopChosen) == True:
+            items = self.listApps2.selectedItems()
+            for item in items:
+                self.timedApps.append(item.text())
+            self.appDetect2.updateListAppView(self.timedApps)    
+            self.timeStopChosen = False
+            self.anAppChosenToStop = False
+        else:
+            self.showDialog()
+    
+    def showDialog(self):
+        msgBox = QMessageBox()
+        msgBox.setIcon(QMessageBox.Warning)
+        msgBox.setWindowTitle("WARNING")
+        msgBox.setStandardButtons(QMessageBox.Ok)
+        if (self.anAppChosenToStop == False) and (self.timeStopChosen == False):
+            msgBox.setText("Please select a time and an app")
+        elif (self.anAppChosenToStop == True) and (self.timeStopChosen == False):
+            msgBox.setText("Please select a time")
+        else: 
+            msgBox.setText("Please select an app")
+        msgBox.exec()
     
     
-            
-    
-    
-    #item.setFlags(Qt.NoItemFlags) #grey out an item
-
 
 
         
